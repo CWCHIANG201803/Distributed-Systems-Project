21f829b02ca571aea02c40c0203868f682193128
Hsing Jung Chiu <hsingjungc@student.unimelb.edu.au>
2020-09-07 11:07:26 +0800

    Finish keep alive protocol.

--- src/main/java/pb/protocols/keepalive/KeepAliveProtocol.java
+++ src/main/java/pb/protocols/keepalive/KeepAliveProtocol.java
@@ -46,6 +46,17 @@ public class KeepAliveProtocol extends Protocol implements IRequestReplyProtocol
 	 */
 	public static final String protocolName="KeepAliveProtocol";
 	
+    /*
+     * Indicate if the connection between this endpoint and the other one is
+     * alive or not.
+     */
+    private boolean isAlive = false;
+
+    /*
+     * Indicate if the protocol is running or not.
+     */
+    private boolean protocolRunning = false;
+
 	/**
 	 * Initialise the protocol with an endopint and a manager.
 	 * @param endpoint
@@ -64,11 +75,15 @@ public class KeepAliveProtocol extends Protocol implements IRequestReplyProtocol
 	}
 
 	/**
-	 * 
+	 * Make the protocol not send any more messages or raise any more protocol
+     * events.
 	 */
 	@Override
 	public void stopProtocol() {
-		
+		if(protocolRunning) {
+            log.severe("protocol stopped while it is still underway");
+            protocolRunning = false;
+        }
 	}
 	
 	/*
@@ -76,61 +91,96 @@ public class KeepAliveProtocol extends Protocol implements IRequestReplyProtocol
 	 */
 	
 	/**
-	 * 
+	 * Call checkClientTimeout() to set up a timer
+     * to set up a timer.
 	 */
 	public void startAsServer() {
-		
-	}
+        protocolRunning = true;
+	    checkClientTimeout();
+    }
 	
 	/**
-	 * 
+	 * Call {@link pb.Manager#endpointTimedOut(Endpoint,Protocol)} if the server does
+     * not receive a KeepAlive request at least every 20 seconds.
 	 */
 	public void checkClientTimeout() {
-		
+        Utils.getInstance().setTimeout(()->{
+            log.info(String.valueOf(protocolRunning));
+            if(!protocolRunning) {
+                //do not send any more messages or raise any more protocol events
+            }
+            else if(isAlive) {
+                isAlive = false;
+                startAsServer();
+            }
+            else {
+                manager.endpointTimedOut(endpoint, this);
+            }
+        },20000);
 	}
 	
 	/**
-	 * 
+	 * Call {@link pb.protocols.keepalive.KeepAliveProcol#sendRequest(Message)} to
+     * send a request every 20 second.
 	 */
-	public void startAsClient() throws EndpointUnavailable {
-		
-	}
+    public void startAsClient() throws EndpointUnavailable {
+        protocolRunning = true;
+        KeepAliveRequest keepAliveRequest = new KeepAliveRequest();
+        sendRequest(keepAliveRequest);
+        Utils.getInstance().setTimeout(()->{
+            if(isAlive) {
+                try {
+                    isAlive = false;
+                    startAsClient();
+                } catch(EndpointUnavailable e) {
+                }
+            }
+            else if(!protocolRunning) {
+                //do not send any more messages or raise any more protocol events
+            }
+            else {
+                manager.endpointTimedOut(endpoint, this);
+            }
+        },20000);
+    }
 
 	/**
-	 * 
+	 * Send a request.
 	 * @param msg
 	 */
 	@Override
 	public void sendRequest(Message msg) throws EndpointUnavailable {
-		
+        endpoint.send(msg);
 	}
 
 	/**
-	 * 
+	 * Set isAlive to true if receive a reply.
 	 * @param msg
 	 */
 	@Override
-	public void receiveReply(Message msg) {
-		
-	}
+	public void receiveReply(Message msg) throws EndpointUnavailable {
+        isAlive = true;
+    }
 
 	/**
-	 *
+	 * Set isAlive to true if receive a request.
 	 * @param msg
 	 * @throws EndpointUnavailable 
 	 */
 	@Override
 	public void receiveRequest(Message msg) throws EndpointUnavailable {
-		
-	}
+        KeepAliveReply keepAliveReply = new KeepAliveReply();
+        sendReply(keepAliveReply);
+	    isAlive = true;
+    }
 
 	/**
-	 * 
+	 * Send a reply.
 	 * @param msg
 	 */
 	@Override
 	public void sendReply(Message msg) throws EndpointUnavailable {
-		
+        endpoint.send(msg);
 	}
 	
 	
